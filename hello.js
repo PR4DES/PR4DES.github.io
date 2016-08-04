730536c08db13c4615a07afe9feeb7b4725a140e
PR4DES <qwe52165@gmail.com>
2016-08-04 20:20:53 +0900

    sadf

---
+++ hello.js
@@ -0,0 +1,7 @@
+var HeaderMouseIn = function(e) { 
+	e.style.backgroundColor = "rgba(255,255,255,0.8)";
+}
+var HeaderMouseOut = function(e) {
+	e.style.backgroundColor = "rgba(255,255,255,0.5)";
+}
+
