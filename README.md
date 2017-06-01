b61406c9b6bca4217ef18c03d68ad3a4c9a37243
PR4DES <qwe52165@gmail.com>
2017-06-02 00:26:31 +0900

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+https://pr4des.github.io/
--- index.php
+++ index.php
@@ -6,48 +6,57 @@
 	<link rel="stylesheet" type="text/css" href="./main.css"/>
 </head>
 <body>
-	<div class="information">
+	<div class="me">
 		<p id="name">SONGYI LEE</p>
 		<p id="email">qwe52165@unist.ac.kr</p>
 	</div>
-	<div class="interest">
-		<p>INTEREST</p>
+	<div class="website">
+		<a href="https://github.com/PR4DES" target="_blank"><img src="http://egyptiankarim.com/icons/github/github-512-black.png" id="github"/></a>
 	</div>
-	<div class="language">
-		<p>LANGUAGE</p>
-		<p>Proficient in C++, java, python</p>
-	</div>
-	<div class="education">
-		<p>EDUCATION</p>
-		<div id="UNIST">
-			<p>From 02/2014 to Current	UNIST	Ulsan,Korea</p>
-			<p>Major in Computer Science & Engineering</p>
-			<p>Minor in Electronical Engineering</p>
-			<p>Expected Date of Graduation : February, 2018</p>
+	<hr />
+	<div class="information">
+		<div class="interest">
+			<p id="title">INTEREST</p>
+			<div id="content"></div>
+		</div>
+		<div class="language">
+			<p id="title">LANGUAGE</p>
+			<p id="content">Proficient in C++, java, python</p>
+		</div>
+		<div class="education">
+			<p id="title">EDUCATION</p>
+			<div id="content">
+				<div id="UNIST">
+					<p id="date">From 02/2014 to Current	UNIST	Ulsan,Korea</p>
+					<p>Major in Computer Science & Engineering</p>
+					<p>Minor in Electronical Engineering</p>
+					<p>Expected Date of Graduation : February, 2018</p>
+				</div>
+			</div>
+		</div>
+		<div class="experience">
+			<p id="title">EXPERIENCE</p>
+			<div id="content">
+				<div id="SSL">
+					<p id="date">From 12/2015 to Current	SSL(System Software Lab)	Ulsan,Korea</p>
+					<p>Position Held : B.S Student Research Intern</p>
+					<p>Virtual Machine, Operating System</p>
+				</div>
+				<div id="Planer">
+					<p id="date">From 3/2015 to 12/2015	Planer	Ulsan,Korea</p>
+					<p>Position Held : Front-end Web Developer</p>
+					<p>Small startup company supported by startup program in UNIST</p>
+				</div>
+			</div>
+		</div>
+		<div class="activity">
+			<p id="title">ACTIVITIES</p>
+			<div id="content">
+				<p>Head of club HeXA hacking department in 2015.</p>
+				<p>POX(Power of XX) women hacking competition 2014 Finalist as a team HeXA.</p>
+				<p>HeXATHON 2014 operation commitee member.</p>
+			</div>
 		</div>
 	</div>
-	<div class="experience">
-		<div id="SSL">
-			<p>From 12/2015 to Current	SSL(System Software Lab)	Ulsan,Korea</p>
-			<p>Position Held : B.S Student Research Intern</p>
-			<p>Virtual Machine, Operating System</p>
-		</div>
-		<div id="Planer">
-			<p>From 3/2015 to 12/2015	Planer	Ulsan,Korea</p>
-			<p>Position Held : Front-end Web Developer</p>
-			<p>Small startup company supported by startup program in UNIST</p>
-		</div>
-	</div>
-	<div class="activity">
-		<p>ACTIVITIES</p>
-		<p>Head of club HeXA hacking department in 2015.</p>
-		<p>POX(Power of XX) women hacking competition 2014 Finalist as a team HeXA.</p>
-		<p>HeXATHON 2014 operation commitee member.</p>
-	</div>
-	<div class="reference">
-		<a href="http://www.dailysecu.com/news/articleView.html?idxno=8018">http://www.dailysecu.com/news/articleView.html?idxno=8018</a>
-		<a href="https://www.facebook.com/HeXATHON/">https://www.facebook.com/HeXATHON/</a>
-	</div>
-
 </body>
 </html>
