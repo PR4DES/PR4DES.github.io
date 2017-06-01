6b25322781ef31fbf73d445b472474227699e5c8
PR4DES <qwe52165@gmail.com>
2017-06-02 00:33:31 +0900

    .

--- hello.js
+++
@@ -1,7 +0,0 @@
-var HeaderMouseIn = function(e) { 
-	e.style.backgroundColor = "rgba(255,255,255,0.8)";
-}
-var HeaderMouseOut = function(e) {
-	e.style.backgroundColor = "rgba(255,255,255,0.5)";
-}
-
